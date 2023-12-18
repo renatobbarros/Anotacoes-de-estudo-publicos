404e9824d011f7caa905fbd90496c1d7562718db
Renato Barros <renatohenriquebarros2@gmail.com>
2023-12-18 15:24:45 -0300

    Adição de itens iniciais

---
+++ c#/Codigos-feitos-assistindo-aulas/testes2/Program.cs
@@ -0,0 +1,15 @@
+﻿using testes2.Models;
+
+Pessoa p1 = new Pessoa();
+p1.nome = "Joao";
+
+Pessoa p2 = new Pessoa();
+p2.nome = "Carlos";
+
+Curso cursoDeIngles = new Curso();
+cursoDeIngles.Nome = "Inglês";
+cursoDeIngles.Alunos = new List<Pessoa>();
+
+cursoDeIngles.AdicionarAluno(p1);
+cursoDeIngles.AdicionarAluno(p2);
+cursoDeIngles.ListarAlunos();
